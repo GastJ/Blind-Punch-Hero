68b64f870371e1aa580afb6ccc706ab1efe280b8
jgast <gastineau.julien@hotmail.fr>
2016-12-12 14:52:38 +0100

    Initialisation de phaser

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,22 @@
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
+let w = window.innerWidth;
+let h = window.innerHeight;
+let game = new Phaser.Game(w, h, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render});
 function preload() {
+
+	console.log("test");
+	game.load.image("gameBg","assets/background.png", w, h);
 }
 
+
 function create() {
+	game.physics.startSystem(Phaser.Physics.ARCADE); 
+	// fond
+    game.add.tileSprite(0,0,w,h,'gameBg');
 }
 
 function update() {
+}
+
+function render() {
+
 }
